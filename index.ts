9c3c68182a3fb59e9dbb86fe1535db7b38effcfb
Zellar dev <zellardev@MacBook-Pro-de-Zellar.local>
2021-01-13 21:20:19 -0300

    finalizando evento

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Application } from "./application";
 import * as mongoose from 'mongoose';
 
+
 mongoose
     .connect(process.env.connectionString?.toString()!, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(_ => {
